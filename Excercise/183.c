84b2d8034fbc2dce1365c23fbf3a7cf14ca692b0
SEORRIE <jiphacker0123@gmail.com>
2024-12-21 16:56:34 +0900

    Create 183.c
    코드 수정중

---
+++ Excercise/183.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int star(int n)
+{
+    if(){
+        return 0;
+    }
+    else {
+        for(int i = 1, i <= 5 ; i++){
+            for(int j = 1, j <= 5 ; j++){
+                if(i == 1 || i == j || i - 1 == j || i = j - 1){
+                    printf("*");
+                }
+                else
+            }
+        }
+    }
+}
+
+int main()
+{
+    int i;
+    scanf("%d", &i);
+    star(i);
+}
